ecd8398db0dbfbeae16661b9494da6aa209ea40b
Alex <Alex@ALEX-PC>
2018-11-11 21:57:37 +0200

    Make Test servlet

---
+++ MyTestWebApp/src/info/sjd/service/SomeService.java
@@ -0,0 +1,8 @@
+package info.sjd.service;
+
+public class SomeService {
+
+	public static String getSomeMopdifiedText(String name, String pass) {
+		return "Your name is " + name + " and password is " + pass;
+	}
+}
